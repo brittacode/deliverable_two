dd0d46be6444836734cd36916ef0a780002d6157
brittany <brioden88@gmail.com>
2021-07-21 11:13:58 -0400

    deliverables part two

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let petName = prompt("What is yout pets name");
+let action = prompt ("walk, pet or feed");
+let total = 0; 
+
+for (let i = 0; i > action; i++ ){
+    let choice = Number(  prompt ("walk, pet or feed?") );
+    total += choice;
+    let pet =+ 2;
+    let walk =+2;
+    let feed =-1;
+
+}
+
+console.log ("Your pets energy level is "  + total  +  " your pets happiness is " + total + ".");
+
+
