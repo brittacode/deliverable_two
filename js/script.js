5d887327ce9f459b33e2baca1c302146a17ed2a3
brittany <brioden88@gmail.com>
2021-07-26 09:42:42 -0400

    updated code

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
 let petName = prompt("What is yout pets name");
-let action = prompt ("walk, pet or feed");
-let total = 0; 
+let choice = prompt ("walk, pet or feed");
 
-for (let i = 0; i > action; i++ ){
-    let choice = Number(  prompt ("walk, pet or feed?") );
-    total += choice;
-    let pet =+ 2;
-    let walk =+2;
-    let feed =-1;
+let energy = 0; 
+let happiness = 0;
 
+for  ( i = 0; i < 6; i++ ){
+    var action = prompt ("walk, pet or feed?" );
+    while (action === 0 & action === "") { action = prompt ("walk, pet or feed?");
 }
 
-console.log ("Your pets energy level is "  + total  +  " your pets happiness is " + total + ".");
+while  (action === "walk" , energy > 0 ) {happiness +=2, energy -=1; }
+
+ if (action === "pet") {happiness +=1; }
+
+else if (action === "feed") {energy = energy +=2; }
+    
+else if (energy === 0) { alert("I'm too tired");}
+
+}
+{
+happiness = (happiness + 2);
+energy = (energy - 1); }
+
+console.log ("Your pets energy", energy, "your pets happiness is", happiness);
 
 
